e388a748f716978a3e86fd4f4a9213951e176119
Nikolay Gromov <nicothin@gmail.com>
2016-04-01 15:28:59 +0300

    Стрелка внизу «первого экрана» (10 мин, исправление предыдущего коммита)

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,3 +1,16 @@
 $( document ).ready(function() {
 
+  // показ/сокрытие белой херовины со стрелкой на «первом экране»
+  var t0;
+  $(window).on('scroll', function(){
+    clearTimeout(t0);
+    t0 = setTimeout(function () {
+        if($(document).scrollTop() > 100) {
+          $('.first-screen__to-bottom').fadeOut(150);
+        }
+        else {
+          $('.first-screen__to-bottom').fadeIn(150);
+        }
+      }, 100);
+  });
 });
